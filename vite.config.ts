66c0bad21c2e99556fcf368fcc7772e15d25c392
dom-l07 <dominiclim207@gmail.com>
2025-11-22 23:08:02 +0800

    updated to use cert

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import fs from 'fs'
 
 const devtoolsEventBusPort = Number(
   process.env.TANSTACK_EVENT_BUS_PORT ?? '42069',
@@ -26,6 +27,14 @@ const config = defineConfig({
     tanstackStart(),
     viteReact(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
+  },
 })
 
 export default config
